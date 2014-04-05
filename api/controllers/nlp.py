2a0fee21fb02b676cf4b7f4c011340ba5540126b
Andrei Demian <demian.andrei@yahoo.com>
2014-04-06 02:35:08 +0300

    Update NLP thingie

--- api/controllers/nlp.py
+++ api/controllers/nlp.py
@@ -13,7 +13,7 @@ from gensim import corpora, models, similarities
 import urllib
 import json
 import datetime
-import pprint
+
 
 @app.route('/nlp/sentiment')
 def nlp_sentiment():
@@ -54,20 +54,24 @@ def npl_rank_keywords():
 @app.route('/nlp/similar')
 def nlp_similar():
     documents = [
-        "This is not good at all",
         "Human machine interface for lab abc computer applications",
-        # "A survey of user opinion of computer system response time",
-        # "The EPS user interface management system",
-        # "System and human system engineering testing of EPS",
-        # "Relation of user perceived response time to error measurement",
-        # "The generation of random binary unordered trees",
-        # "The intersection graph of paths in trees",
-        # "Graph minors IV Widths of trees and well quasi ordering",
+        "A survey of user opinion of computer system response time",
+        "The EPS user interface management system",
+        "System and human system engineering testing of EPS",
+        "Relation of user perceived response time to error measurement",
+        "The generation of random binary unordered trees",
+        "The intersection graph of paths in trees",
+        "Graph minors IV Widths of trees and well quasi ordering",
         "Graph minors A survey"]
 
     # remove common words and tokenize
     stoplist = set('for a of the and to in by from on with as a'.split())
     texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
+
+    # remove words that appear only once
+    all_tokens = sum(texts, [])
+    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+    texts = [[word for word in text if word not in tokens_once] for text in texts]
 
     # Create dictionary
     dictionary = corpora.Dictionary(texts)
@@ -76,14 +80,14 @@ def nlp_similar():
     corpus = [dictionary.doc2bow(text) for text in texts]
 
     # Define LSI space
-    lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=10)
+    lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
 
     # Get similarity of teh doc vs documents
     doc = "Human computer interaction"
     vector = dictionary.doc2bow(doc.lower().split())
-    vector_lsi = lsi[vector]
+    vector_lsi = lsi[vector] # convert the query to LSI space
 
-    index = similarities.MatrixSimilarity(lsi[corpus])
-    sims = index[vector_lsi]
+    index = similarities.MatrixSimilarity(lsi[corpus]) # transform corpus to LSI space and index it
+    sims = index[vector_lsi] # perform a similarity query against the corpus
 
     return Response(json.dumps(sims), mimetype='application/json')
