90639ac3e36422aff8515ab7ff225e1269b1fd77
Andrei Demian <demian.andrei@yahoo.com>
2014-04-06 00:28:14 +0300

    Add similarity to nlp endpoint

--- api/controllers/nlp.py
+++ api/controllers/nlp.py
@@ -8,6 +8,7 @@ from flask import render_template
 from api.core import app, db
 from api.models.user import User
 from api.models.page import Page
+from gensim import corpora, models, similarities
 
 import urllib
 import json
@@ -49,3 +50,45 @@ def npl_rank_keywords():
     return Response(json.dumps({
         'accounts': ranked
     }), mimetype='application/json')
+
+@app.route('/nlp/similar')
+def nlp_similar():
+   documents = [
+        "Human machine interface for lab abc computer applications",
+        "A survey of user opinion of computer system response time",
+        "The EPS user interface management system",
+        "System and human system engineering testing of EPS",
+        "Relation of user perceived response time to error measurement",
+        "The generation of random binary unordered trees",
+        "The intersection graph of paths in trees",
+        "Graph minors IV Widths of trees and well quasi ordering",
+        "Graph minors A survey"]
+
+    # remove common words and tokenize
+    stoplist = set('for a of the and to in'.split())
+    texts = [[word for word in document.lower().split() if word not in stoplist]
+            for document in documents]
+
+    # remove words that appear only once
+    all_tokens = sum(texts, [])
+    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+    texts = [[word for word in text if word not in tokens_once]
+            for text in texts]
+
+    # Create dictionary
+    dictionary = corpora.Dictionary(texts)
+
+    # Define corpus
+    corpus = [dictionary.doc2bow(text) for text in texts]
+
+    # Define LSI space
+    lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
+
+    # Get similarity of teh doc vs documents
+    doc = "Human computer interaction"
+    vec_bow = dictionary.doc2bow(doc.lower().split())
+    vec_lsi = lsi[vec_bow] # convert the query to LSI space
+
+    index = similarities.MatrixSimilarity(lsi[corpus]) # transform corpus to LSI space and index it
+    sims = index[vec_lsi] # perform a similarity query against the corpus
+    return(Response(sims))
