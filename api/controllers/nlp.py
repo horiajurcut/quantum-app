3dd464cabbc9d06d9c853ff2c28f96c9a79f091e
Andrei Demian <demian.andrei@yahoo.com>
2014-04-06 04:37:04 +0300

    Update NLP thingie

--- api/controllers/nlp.py
+++ api/controllers/nlp.py
@@ -57,22 +57,11 @@ def match_similar(inputs, questions):
     stoplist = set('for a of the and to in by from on with as a'.split())
     texts = [[word for word in question.lower().split() if word not in stoplist] for question in questions]
 
-    print texts
-
-    # remove words that appear only once
-    all_tokens = sum(texts, [])
-    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-    texts = [[word for word in text if word not in tokens_once] for text in texts]
-
-    print texts
-
     # Create dictionary
     dictionary = corpora.Dictionary(texts)
 
     # Define corpus
     corpus = [dictionary.doc2bow(text) for text in texts]
-
-    print questions
 
     # Define LSI space
     lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
