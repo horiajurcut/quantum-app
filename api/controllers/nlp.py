17140e0482d0a09879f5f7f5a757b1ae03e59ee4
Andrei Demian <demian.andrei@yahoo.com>
2014-04-06 04:27:26 +0300

    Update NLP thingie

--- api/controllers/nlp.py
+++ api/controllers/nlp.py
@@ -57,27 +57,27 @@ def match_similar(inputs, questions):
     stoplist = set('for a of the and to in by from on with as a'.split())
     texts = [[word for word in question.lower().split() if word not in stoplist] for question in questions]
 
-    # remove words that appear only once
-    all_tokens = sum(texts, [])
-    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-    texts = [[word for word in text if word not in tokens_once] for text in texts]
+    # # remove words that appear only once
+    # all_tokens = sum(texts, [])
+    # tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+    # texts = [[word for word in text if word not in tokens_once] for text in texts]
 
-    # Create dictionary
-    dictionary = corpora.Dictionary(texts)
+    # # Create dictionary
+    # dictionary = corpora.Dictionary(texts)
 
-    # Define corpus
-    corpus = [dictionary.doc2bow(text) for text in texts]
+    # # Define corpus
+    # corpus = [dictionary.doc2bow(text) for text in texts]
 
-    # Define LSI space
-    lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=100)
+    # # Define LSI space
+    # lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=100)
 
-    # Get similarity of teh doc vs documents
-    vector = dictionary.doc2bow(inputs.lower().split())
-    vector_lsi = lsi[vector]
+    # # Get similarity of teh doc vs documents
+    # vector = dictionary.doc2bow(inputs.lower().split())
+    # vector_lsi = lsi[vector]
 
-    index = similarities.MatrixSimilarity(lsi[corpus])
-    sims = index[vector_lsi]
-    return sims
+    # index = similarities.MatrixSimilarity(lsi[corpus])
+    # sims = index[vector_lsi]
+    # return sims
 
 def match_group(inputs, groups, min_threshold):
 
