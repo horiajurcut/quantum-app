a2e9ed3f197044ac9893d2f3701bc812d63ce052
Andrei Demian <demian.andrei@yahoo.com>
2014-04-06 05:35:53 +0300

    Update NLP thingie

--- api/controllers/nlp.py
+++ api/controllers/nlp.py
@@ -54,10 +54,18 @@ def npl_rank_keywords():
 def match_similar(inputs, questions):
 
     # remove common words and tokenize
-    stoplist = set('for of the and to in by from on with as'.split())
+    stoplist = set('for a of the and to in'.split())
     texts = [[word for word in question.lower().split() if word not in stoplist] for question in questions]
 
     print texts
+
+     # remove words that appear only once
+    all_tokens = sum(texts, [])
+    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+    texts = [[word for word in text if word not in tokens_once] for text in texts]
+
+    print texts
+
 
     # Create dictionary
     dictionary = corpora.Dictionary(texts)
