8b4250e72b76cda4af5bbb58af11bc092ce46d6d
Horia Jurcut <horia.dragos@gmail.com>
2014-04-06 09:09:54 +0300

    Prop commit

--- api/controllers/nlp.py
+++ api/controllers/nlp.py
@@ -57,10 +57,9 @@ def match_similar(inputs, questions):
     texts = [[word for word in question.lower().split() if word not in stoplist] for question in questions]
 
     # remove words that appear only once
-    all_tokens = sum(texts, [])
-    raise Exception(all_tokens)
-    tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-    texts = [[word for word in text if word not in tokens_once] for text in texts]
+    #all_tokens = sum(texts, [])
+    #tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+    #texts = [[word for word in text if word not in tokens_once] for text in texts]
 
     # Create dictionary
     dictionary = corpora.Dictionary(texts)
