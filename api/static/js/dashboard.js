55ce15274839e35d166be4456710470fc5ad9e1e
Andrei Demian <demian.andrei@yahoo.com>
2014-04-06 12:50:34 +0300

    Answer

--- api/static/js/dashboard.js
+++ api/static/js/dashboard.js
@@ -8,30 +8,20 @@ function showQuestionsModal() {
 
 	$modalBackground.on('click', closeQuestionsModal);
 
-	var template = $('#modal-question').html();
-	var data = {
-		question: "I love when actors actually have--and run--their own FB pages. You were already one of my favorite actors and seeing stuff like this only makes me love you more!",
-		askedby : [
-			{
-				full_name: 'Stefan Filip',
-				user_id: 'stefy.filip',
-				avatar: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/t1.0-1/p160x160/1975001_10152232954170395_510883959_n.jpg'
-			},
-			{
-				full_name: 'Horia Jurcut',
-				user_id: 'horiajurcut',
-				avatar: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash2/t1.0-1/c0.45.160.160/p160x160/1391726_10151966902849082_288564753_n.jpg'
-			},
-			{
-				full_name: 'Giorgiana Petre',
-				user_id: 'giorgiana.petre',
-				avatar: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/t1.0-1/c0.29.160.160/p160x160/1010306_10202683853310726_2016244149_n.jpg'
-			}
-		]	
-	};
+	var groupId = $(this).attr('data-group-id');
 
-	$modal.html(Mustache.to_html(template, data));
-	$('.modal-close-button').on('click', closeQuestionsModal);
+	$.ajax({
+        url: "/dashboard/" + groupId + "/details",
+        type: "GET",
+        success: function(data) {
+        	var template = $('#modal-question').html();
+
+			$modal.html(Mustache.to_html(template, data));
+			$('.modal-close-button').on('click', closeQuestionsModal);
+    	},
+	    dataType: "json",
+    	timeout: 2000
+    });
 
 	return false;
 }
@@ -76,7 +66,7 @@ function polling() {
 					}
 				}
 
-				$('.questions-list tbody').append('<tr>\
+				$('.questions-list tbody').append('<tr data-group-id="' + value.id + '">\
 					<td class="sentiment ' + sentiment + '"></td>\
 					<td class="question">' + value.question + '</td>\
 		            <td class="frequency"><span>' + value.frequency + '</span></td>\
