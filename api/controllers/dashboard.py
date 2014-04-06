c762893581cd0646bb9e3357a3dd7bf2ed01bfd7
Horia Jurcut <horia.dragos@gmail.com>
2014-04-06 06:05:31 +0300

    Prop commit

--- api/controllers/dashboard.py
+++ api/controllers/dashboard.py
@@ -66,7 +66,7 @@ def dashboard_publish(event_id):
         'message':      'This is an awesome post. Deal with it!',
         'client_id':    '795829153779723'
     }
-    data = requests.post('https://graph.facebook.com/%s/feed' % page.id, data=json.dumps(params))
+    data = requests.post('https://graph.facebook.com/%s/feed' % page.id, data=params)
     return data.content
 
     return redirect('/dashboard/page/%s' % page.page_id)
