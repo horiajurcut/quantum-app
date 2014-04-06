5603dcf189eb1635094451522e93086f554cbd1a
Stefan Filip <stefan@nextroot.com>
2014-04-06 13:25:30 +0300

    More work on the landing page

--- api/static/js/landing.js
+++ api/static/js/landing.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('.page2').appear();
 	$('.page4').appear();
+	$('.page6').appear();
 
 	$('.page2').on('appear', function() {
 		$('.page2').animate({ opacity: 1 });
@@ -10,5 +11,9 @@ $(document).ready(function() {
 		$('.page4 ul li').each(function(index, element) {
 			$(element).animate({ opacity: 1 }, 800 * index);
 		});
+	});
+
+	$('.page6').on('appear', function() {
+		$('.page6 ul li').addClass('active');
 	});
 });
