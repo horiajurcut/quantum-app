ff8ca4c39c2dbfa773992f6364a7857803d856f4
Horia Jurcut <horia.dragos@gmail.com>
2014-04-06 09:17:23 +0300

    Prop commit

--- api/controllers/nlp.py
+++ api/controllers/nlp.py
@@ -77,7 +77,7 @@ def match_similar(inputs, questions):
     index = similarities.MatrixSimilarity(lsi[corpus])
     sims = index[vector_lsi]
 
-    raise Exception(index)
+    raise Exception(lsi)
 
     return sims
 
